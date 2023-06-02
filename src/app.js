6ed4422ba7fdf48cefb28322f9386b1dd459f690
Oleksandra Tiankina <ag7756862@gmail.com>
2023-06-02 03:28:05 +0300

    refactor: create static field

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const { authRouter, usersRouter, categoriesRouter, productsRouter } = require('.
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/auth', authRouter)
